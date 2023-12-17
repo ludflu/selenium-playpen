ebe09439a8a6e835f47957f61a6a1d8fd87f49c4
Jim Snavely <ludflu@gmail.com>
2023-12-17 15:54:15 -0500

    google scholar scraper

--- scholar.py
+++ scholar.py
@@ -8,11 +8,11 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 #if you want to leave the window open for inspection:
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=chrome_options)
+#chrome_options = Options()
+#chrome_options.add_experimental_option("detach", True)
+#driver = webdriver.Chrome(options=chrome_options)
 
-#driver = webdriver.Chrome()
+driver = webdriver.Chrome()
 
 driver.get("http://scholar.google.com")
 elem = driver.find_element(By.NAME, "q")
@@ -27,7 +27,17 @@ external_link_xpath = "//a[starts-with(@href,'https') and not(contains(@href, 'g
 
 first_link = driver.find_element(By.XPATH,external_link_xpath)
 cnt = first_link.get_attribute("innerHTML")
-print(f"link: {cnt}")
-first_link.click()
+
+
+links = driver.find_elements(By.XPATH,external_link_xpath)
+
+articles = []
+for link in links:
+    href = link.get_attribute("href")
+    content = link.get_attribute("innerHTML")
+    articles.append( (content, href) )
+
+#first_link.click()
+print(articles)
 
 
